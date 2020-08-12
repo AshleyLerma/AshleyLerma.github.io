d391591aff6be9527cc354995c5ec52345feee8a
Ashley Lerma <lerma.scout@gmail.com>
2020-08-12 18:01:34 -0500

    customized header links and info

--- resume/src/components/header/header.js
+++ resume/src/components/header/header.js
@@ -40,57 +40,28 @@ function Header() {
 
       <div className="row banner">
         <div className="banner-text">
-          <h1 className="responsive-headline">I'm Scout Lerma.</h1>
+          <h1 className="responsive-headline">I'm Ashley Lerma.</h1>
           <h3>
-            I am an <span>Austin</span> based <span>Software Developer </span>
-            with a background in event management. Passionate about learning,
-            detail oriented, and self-driven. Currently a student in the
-            University of Texas <span>full-stack development</span> coding
-            bootcamp . Let's{" "}
+            An <span>Austin</span> based <span>Software Developer </span>
+            with a background in event management. Detail oriented, and
+            self-driven with a passion for learning. Enrolled in the{" "}
+            <span>full-stack development</span> coding bootcamp at the
+            University of Texas at Austin. Let's{" "}
             <a className="smoothscroll" href="#about">
               start scrolling
             </a>{" "}
-            and learn more{" "}
-            <a className="smoothscroll" href="#about">
-              about me
-            </a>
             .
           </h3>
           <hr />
           <ul className="social">
             <li>
-              <a href="#">
-                <i className="fa fa-facebook" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-twitter" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-google-plus" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
+              <a href="https://www.linkedin.com/in/ashleylerma/" target="blank">
                 <i className="fa fa-linkedin" />
               </a>
             </li>
             <li>
-              <a href="#">
-                <i className="fa fa-instagram" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-dribbble" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-skype" />
+              <a href="https://github.com/AshleyLerma" target="blank">
+                <i className="fa fa-github" />
               </a>
             </li>
           </ul>
