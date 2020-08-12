7dda79a041e83ed63b49a9ed73e77a1e025e9f5b
Ashley Lerma <lerma.scout@gmail.com>
2020-08-12 17:30:46 -0500

    fixed header render

--- resume/src/components/header/header.js
+++ resume/src/components/header/header.js
@@ -2,184 +2,112 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <header id="home">
-        <nav id="nav-wrap">
-          <a classname="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a classname="mobile-btn" href="#" title="Hide navigation">
-            Hide navigation
-          </a>
-          <ul id="nav" classname="nav">
-            <li classname="current">
-              <a classname="smoothscroll" href="#home">
-                Home
+    <header id="home">
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#" title="Hide navigation">
+          Hide navigation
+        </a>
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#resume">
+              Resume
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#portfolio">
+              Works
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#testimonials">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>{" "}
+        {/* end #nav */}
+      </nav>{" "}
+      {/* end #nav-wrap */}
+      <div className="row banner">
+        <div className="banner-text">
+          <h1 className="responsive-headline">I'm Jonathan Doe.</h1>
+          <h3>
+            I'm a Manila based <span>graphic designer</span>,{" "}
+            <span>illustrator</span> and <span>webdesigner</span> creating
+            awesome and effective visual identities for companies of all sizes
+            around the globe. Let's{" "}
+            <a className="smoothscroll" href="#about">
+              start scrolling
+            </a>
+            and learn more{" "}
+            <a className="smoothscroll" href="#about">
+              about me
+            </a>
+            .
+          </h3>
+          <hr />
+          <ul className="social">
+            <li>
+              <a href="#">
+                <i className="fa fa-facebook" />
               </a>
             </li>
             <li>
-              <a classname="smoothscroll" href="#about">
-                About
+              <a href="#">
+                <i className="fa fa-twitter" />
               </a>
             </li>
             <li>
-              <a classname="smoothscroll" href="#resume">
-                Resume
+              <a href="#">
+                <i className="fa fa-google-plus" />
               </a>
             </li>
             <li>
-              <a classname="smoothscroll" href="#portfolio">
-                Works
+              <a href="#">
+                <i className="fa fa-linkedin" />
               </a>
             </li>
             <li>
-              <a classname="smoothscroll" href="#testimonials">
-                Testimonials
+              <a href="#">
+                <i className="fa fa-instagram" />
               </a>
             </li>
             <li>
-              <a classname="smoothscroll" href="#contact">
-                Contact
+              <a href="#">
+                <i className="fa fa-dribbble" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-skype" />
               </a>
             </li>
           </ul>
-          {/* end #nav */}
-        </nav>
-        {/* end #nav-wrap */}
-        <div classname="row banner">
-          <div classname="banner-text">
-            <h1 classname="responsive-headline">I'm Jonathan Doe.</h1>
-            <h3>
-              I'm a Manila based <span>graphic designer</span>,
-              <span>illustrator</span> and <span>webdesigner</span> creating
-              awesome and effective visual identities for companies of all sizes
-              around the globe. Let's
-              <a classname="smoothscroll" href="#about">
-                start scrolling
-              </a>
-              and learn more
-              <a classname="smoothscroll" href="#about">
-                about me
-              </a>
-              .
-            </h3>
-            <hr />
-            <ul classname="social">
-              <li>
-                <a href="#">
-                  <i classname="fa fa-facebook" />
-                </a>
-              </li>
-              <i classname="fa fa-facebook">
-                <li>
-                  <a href="#">
-                    <i classname="fa fa-twitter" />
-                  </a>
-                </li>
-                <i classname="fa fa-twitter">
-                  <li>
-                    <a href="#">
-                      <i classname="fa fa-google-plus" />
-                    </a>
-                  </li>
-                  <i classname="fa fa-google-plus">
-                    <li>
-                      <a href="#">
-                        <i classname="fa fa-linkedin" />
-                      </a>
-                    </li>
-                    <i classname="fa fa-linkedin">
-                      <li>
-                        <a href="#">
-                          <i classname="fa fa-instagram" />
-                        </a>
-                      </li>
-                      <i classname="fa fa-instagram">
-                        <li>
-                          <a href="#">
-                            <i classname="fa fa-dribbble" />
-                          </a>
-                        </li>
-                        <i classname="fa fa-dribbble">
-                          <li>
-                            <a href="#">
-                              <i classname="fa fa-skype" />
-                            </a>
-                          </li>
-                          <i classname="fa fa-skype"></i>
-                        </i>
-                      </i>
-                    </i>
-                  </i>
-                </i>
-              </i>
-            </ul>
-            <i classname="fa fa-facebook">
-              <i classname="fa fa-twitter">
-                <i classname="fa fa-google-plus">
-                  <i classname="fa fa-linkedin">
-                    <i classname="fa fa-instagram">
-                      <i classname="fa fa-dribbble">
-                        <i classname="fa fa-skype"></i>
-                      </i>
-                    </i>
-                  </i>
-                </i>
-              </i>
-            </i>
-          </div>
-          <i classname="fa fa-facebook">
-            <i classname="fa fa-twitter">
-              <i classname="fa fa-google-plus">
-                <i classname="fa fa-linkedin">
-                  <i classname="fa fa-instagram">
-                    <i classname="fa fa-dribbble">
-                      <i classname="fa fa-skype"></i>
-                    </i>
-                  </i>
-                </i>
-              </i>
-            </i>
-          </i>
         </div>
-        <i classname="fa fa-facebook">
-          <i classname="fa fa-twitter">
-            <i classname="fa fa-google-plus">
-              <i classname="fa fa-linkedin">
-                <i classname="fa fa-instagram">
-                  <i classname="fa fa-dribbble">
-                    <i classname="fa fa-skype">
-                      <p classname="scrolldown">
-                        <a classname="smoothscroll" href="#about">
-                          <i classname="icon-down-circle" />
-                        </a>
-                        <i classname="icon-down-circle"></i>
-                      </p>
-                      <i classname="icon-down-circle"></i>
-                    </i>
-                  </i>
-                </i>
-              </i>
-            </i>
-          </i>
-        </i>
-      </header>
-      <i classname="fa fa-facebook">
-        <i classname="fa fa-twitter">
-          <i classname="fa fa-google-plus">
-            <i classname="fa fa-linkedin">
-              <i classname="fa fa-instagram">
-                <i classname="fa fa-dribbble">
-                  <i classname="fa fa-skype">
-                    <i classname="icon-down-circle"></i>
-                  </i>
-                </i>
-              </i>
-            </i>
-          </i>
-        </i>
-      </i>
-    </div>
+      </div>
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle" />
+        </a>
+      </p>
+    </header>
   );
 }
 export default Header;
